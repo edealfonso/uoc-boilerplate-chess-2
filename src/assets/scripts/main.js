eb4e217ec6c51db270f88d7af02191d51208ae89
edealfonso <elsa.de.alfonso@gmail.com>
2023-06-05 20:56:43 +0200

    beta + aos

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,23 +1,20 @@
 /**
  * Import dependencies from node_modules
- * see commented examples below
  */
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
+import AOS from 'aos';
 
 /**
  * Write any other JavaScript below
  */
 
-import AOS from 'aos';
 
 +(function () {
 
     // AOS initialization + customization
-    // AOS.init({
-    //     once: true,
-    //     duration: 1500,
-    // });
+    AOS.init({
+        once: true,
+        duration: 1500,
+    });
 
 })();
